08b9f1838563f6d4b342431e8ec1a99576e9b116
Limo <christmasboom@mail.ru>
2023-06-17 09:05:53 +0300

    ДЗ готово

--- src/main/java/com/homework24/homework24/controller/CalculatorController.java
+++ src/main/java/com/homework24/homework24/controller/CalculatorController.java
@@ -1,4 +1,50 @@
 package com.homework24.homework24.controller;
 
+import com.homework24.homework24.service.CalculatorServiceImpl;
+import com.sun.jdi.connect.Connector;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
+    private final CalculatorServiceImpl service;
+
+    public CalculatorController(CalculatorServiceImpl service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String getWelcome() {
+        return service.welcome();
+    }
+
+    @GetMapping("/plus")
+    public String getPlus(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return service.plus(num1, num2);
+    }
+      @GetMapping("/minus")
+    public String getMinus(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return service.minus(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String getMultiply(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return service.multiply(num1, num2);
+    }
+    @GetMapping("/divide")
+    public String getDivide(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        return service.divide(num1, num2);
+    }
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMissingParams(MissingServletRequestParameterException ex) {
+        return "Недостаточно параметров";
+    }
+
+
+
 }
--- src/main/java/com/homework24/homework24/service/CalculatorServiceImpl.java
+++ src/main/java/com/homework24/homework24/service/CalculatorServiceImpl.java
@@ -1,4 +1,67 @@
 package com.homework24.homework24.service;
 
-public class CalculatorServiceImpl {
+import com.homework24.homework24.exception.CalculateVoidException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    @Override
+    public String welcome() {
+        return "Добро пожаловать в калькулятор !";
+    }
+
+    @Override
+    public String plus(Double a, Double b) {
+        try {
+            checkArguments(a, b);
+            return "Результат сложения : " + a + " + " + b + " = " + (a + b);
+        } catch (CalculateVoidException e) {
+            e.printStackTrace();
+            throw new CalculateVoidException("Недостаточно параметров");
+        }
+    }
+
+    @Override
+    public String minus(Double a, Double b) {
+        try {
+            checkArguments(a, b);
+            return "Результат вычитания : " + a + " - " + b + " = " + (a - b);
+        } catch (CalculateVoidException e) {
+            e.printStackTrace();
+            return "Недостаточно параметров";
+        }
+    }
+
+    @Override
+    public String multiply(Double a, Double b) {
+        try {
+            checkArguments(a, b);
+            return "Результат умножения : " + a + " * " + b + " = " + (a * b);
+        } catch (CalculateVoidException e) {
+            e.printStackTrace();
+            return "Недостаточно параметров";
+        }
+    }
+
+    @Override
+    public String divide(Double a, Double b) {
+        try {
+            checkArguments(a, b);
+            if (b == 0) {
+                throw new IllegalArgumentException("Делить на ноль нельзя!");
+            }
+            return "Результат деления : " + a + " / " + b + " = " + (a / b);
+        } catch (CalculateVoidException | IllegalArgumentException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    private void checkArguments(Double a, Double b) {
+        if (a == null || b == null) {
+            throw new CalculateVoidException("Недостаточно параметров");
+        }
+
+    }
 }
